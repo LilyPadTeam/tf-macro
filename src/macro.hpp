62b049ff95602898a0dcb69aa1dd45adfd5f77f2
DryFish <realmg51@gmail.com>
2026-08-08 11:33:45 +0700

    Tuff

--- src/macro.hpp
+++ src/macro.hpp
@@ -1,9 +1,13 @@
-// { to }
+
 #define { }
-// } to {
 #define } {
-// if to else
 #define if else
 #define else if
 #define < >
-#define ? !
+#define std tds
+#define >> <<
+#define << >>
+#define :: .
+#define . ::
+#define == !=
+#define != ==
